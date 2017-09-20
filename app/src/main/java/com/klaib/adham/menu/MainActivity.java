0799d7b1459eccc18ae485cfb5bab4b491dc1d86
jomanazene28 <jomana.zene1999@gmail.com>
2017-09-20 10:53:04 +0300

    ss

--- app/src/main/java/com/klaib/adham/menu/MainActivity.java
+++ app/src/main/java/com/klaib/adham/menu/MainActivity.java
@@ -1,5 +1,5 @@
 package com.klaib.adham.menu;
-
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -7,36 +7,34 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import java.util.Calendar;
-
 public class MainActivity extends AppCompatActivity implements View.OnClickListener
-
 {
     private Button btndate;
     private TextView tvshowdate;
-
+    private Object calendar;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         btndate= (Button) findViewById(R.id.btndate);
         tvshowdate= (TextView) findViewById(R.id.tvshowdate);
         btndate.setOnClickListener(this);
-
     }
-
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
        getMenuInflater().inflate(R.menu.main_menu,menu);
         return true;
     }
-
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
         switch (item.getItemId())
         {
             case R.id.mnitmscreen1:
@@ -62,14 +60,30 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
         return true;
     }
-
     @Override
-    public void onClick(View v)
+    public void onClick(View view)
     {
-
-        if (v=btndate)
+        if (view=btndate)
         {
+            //to get current date
             Calendar c= Calendar.getInstance();
+            int year=c.get(Calendar.YEAR);
+            int month=c.get(Calendar.MONTH);
+            final int day=c.get(Calendar.DAY_OF_MONTH);
+            //building data dialog
+            DatePickerDialog.OnDateSetListener dateListner=new DatePickerDialog.OnDateSetListener() {
+                @Override
+                public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                    tvshowdate.setText(dayOfMonth+"/"+month+"/"+year+);
+
+                }
+            };
+            DatePickerDialog datePickerDialog=new DatePickerDialog(this,dateListner,year,month,day);
+
+
+
+
+
         }
 
 
